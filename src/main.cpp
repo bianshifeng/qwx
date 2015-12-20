090c32135fcccb7bef9877ce4927afba0d4e8f7b
Leslie Zhai <xiangzhai83@gmail.com>
2015-12-20 10:38:58 +0800

    Qt5 session management issue

--- src/main.cpp
+++ src/main.cpp
@@ -39,8 +39,9 @@ int main(int argc, char* argv[])
     app.setApplicationVersion(APPLICATION_VERSION);
     // TODO: session management testcase
     // http://marc.info/?l=kde-core-devel&m=144832700109449
-    // KDE-BUG 341930
-    app.setQuitOnLastWindowClosed(false);
+    // KDEBUG-341930
+    // perhaps we need to wait Qt5.9.x to fix it ;-)
+    //app.setQuitOnLastWindowClosed(false);
 
     I18N i18n(TRANSLATIONS_PATH, APPLICATION_ENCODING);
     i18n.translate();
